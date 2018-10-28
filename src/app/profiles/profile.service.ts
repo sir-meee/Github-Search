57a198e6644eaaab2b6db6746c99124d58dbad71
sir-meee <sammymbevi@gmail.com>
2018-10-28 21:00:09 +0300

    Import environment

--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {User} from '../user';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
  @Injectable({
   providedIn: 'root'
 })
