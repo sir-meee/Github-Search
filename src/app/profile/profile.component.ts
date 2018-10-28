437d30ef7f3ff7d6991a5876a9dfe04eef43a56f
sir-meee <sammymbevi@gmail.com>
2018-10-28 21:03:45 +0300

    Import profile service

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,24 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../user';
+import {ProfileService} from '../profiles/profile.service';
  @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
+  providers:[ProfileService]
 })
 export class ProfileComponent implements OnInit {
     user: User;
-   constructor(private http: HttpClient) { }
+   constructor(private profileService: ProfileService) { }
    ngOnInit() {
-    interface ApiResponse {
-        username: string;
-        token;
-    }
-    this.http.get<ApiResponse>('https://api.github.com/users/' + this.user.username + '?access_token=' + this.user.token ).subscribe(data=>{
-      this.user = new User (data.username, data.token) ;
-    }, err => {
-        this.user = new User ('https://api.github.com/users/daneden?access_token=1e09e1baa573d7f49014ffe3db068ea7ce8475cd');
-        console.log('Error occured');
-  });
+    this.profileService.getProfileInfo();
+    this.user = this.profileService.user;
  }
  }
