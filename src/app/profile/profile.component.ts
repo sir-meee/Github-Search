b668812f2f7d5743ffec6b9bc4ca233a53d61d89
sir-meee <sammymbevi@gmail.com>
2018-10-28 20:53:00 +0300

    Makes Http request on component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../user';
  @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-   constructor() { }
+    user: User;
+   constructor(private http: HttpClient) { }
    ngOnInit() {
-  }
+    interface ApiResponse {
+        username: string;
+        token;
+    }
+    this.http.get<ApiResponse>('https://api.github.com/users/' + this.user.username + '?access_token=' + this.user.token ).subscribe(data=>{
+      this.user = new User (data.username, data.token) ;
+  });
+ }
  }
