6f9976e20c7d5cee477d403cc3e28a6e0b372162
sir-meee <sammymbevi@gmail.com>
2018-10-28 22:54:16 +0300

    fix errors

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {User} from '../user';
-import {ProfileService} from '../profiles/profile.service';
- @Component({
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Searching} from '../searching';
+import { ProfileService } from '../profiles/profile.service';
+
+@Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.css'],
-  providers: [ProfileService]
+
 })
 export class ProfileFormComponent implements OnInit {
-user:User;
-  constructor(private profileService: ProfileService) { }
-// findProfile(){
-//   this.profileService.updateProfile(this.user.username);
-// }
+  newSearching = new Searching('');
+  @Output() getName = new EventEmitter<Searching>();
+   submitSearch(name) {
+      this.getName.emit(this.newSearching);
+      this.newSearching = new Searching('');
+      console.log(name.value);
+      this.myService.getProfileInfo(name.value);
+  }
+   constructor(public myService: ProfileService) { }
   ngOnInit() {
   }
  }
