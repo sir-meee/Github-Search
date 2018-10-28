d877604e0c9e1f291700fa1ca0a9dbf9d55cb703
sir-meee <sammymbevi@gmail.com>
2018-10-28 21:10:29 +0300

    add form

---
+++ src/app/profile-form/profile-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import {ProfileService} from '../profiles/profile.service';
+ @Component({
+  selector: 'app-profile-form',
+  templateUrl: './profile-form.component.html',
+  styleUrls: ['./profile-form.component.css'],
+  providers: [ProfileService]
+})
+export class ProfileFormComponent implements OnInit {
+user:User;
+  constructor(private profileService: ProfileService) { }
+findProfile(){
+  this.profileService.updateProfile(this.user.username);
+}
+  ngOnInit() {
+  }
+ }
