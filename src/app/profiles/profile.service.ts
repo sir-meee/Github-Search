82b6b29d7a4bd65d34c1df45652cd5ab40da3c9d
sir-meee <sammymbevi@gmail.com>
2018-10-28 20:59:04 +0300

    Add promise

--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 import {User} from '../user';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
  @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-   constructor() { }
+    user: User;
+  constructor(private http: HttpClient) {
+    this.user = new User (' ', '1e09e1baa573d7f49014ffe3db068ea7ce8475cd'  );
+  }
+   getProfileInfo() {
+    interface ApiResponse {
+      username: string;
+      token;
+  }
+  let promise =new Promise((resolve, reject) => {
+    this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response => {
+         this.user.username = response.username;
+        this.user.token = response.token;
+         resolve();
+    },
+    error => {
+            this.user.username = 'daneden';
+            this.user.token = '1e09e1baa573d7f49014ffe3db068ea7ce8475cd';
+            reject(error);
+        }
+    );
+});
+return promise;
+}
 }
