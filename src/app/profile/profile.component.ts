64b1b21be10e188937f19546aded1551cdb5d50e
sir-meee <sammymbevi@gmail.com>
2018-10-29 06:53:17 +0300

    update profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,10 +11,7 @@ import {ProfileService} from '../profiles/profile.service';
 export class ProfileComponent implements OnInit {
     public username = 'sir-meee';
     user: User;
-    findUser(username) {
-        this.username  = username;
-        this.ngOnInit();
-      }
+    
    constructor(private profileService: ProfileService) { }
    ngOnInit() {
     this.profileService.getProfileInfo(this.username);
