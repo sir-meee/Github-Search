fbeb9aad2ff905981798ebe87bc8917bb5858af2
sir-meee <sammymbevi@gmail.com>
2018-10-29 09:44:59 +0300

    Adds routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { ProfileComponent } from './profile/profile.component';
 import {ProfileService} from './profiles/profile.service';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import {FormsModule} from '@angular/forms';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { RouterModule, Routes } from '@angular/router';
+ const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    ProfileFormComponent
+    ProfileFormComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
